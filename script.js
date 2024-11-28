17b7528bd8cc374f54c082a6f60bd937e16e5aef
Winnss <imubaroks07@gmail.com>
2024-11-28 20:47:41 +0700

    frist main

---
+++ script.js
@@ -0,0 +1,35 @@
+var aText = new Array(
+    "ANDA PUSING DENGAN KISI-KISI FISIKA? YA SAYA JUGA SAMA KAWAN😭",
+    );
+    var iSpeed = 100;
+    var iIndex = 0; 
+    var iArrLength = aText[0].length; 
+    var iScrollAt = 20; 
+     
+    var iTextPos = 0; 
+    var sContents = ''; 
+    var iRow; 
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+
+    typewriter();
